2d0068393d679bd3ee2874b27d655f805e357ec5
asus <qixinye-s@inspur.com>
2025-10-20 23:12:09 +0800

    添加CLI命令行参数配置

--- src/main/java/com/codemind/cli/CodeMindCli.java
+++ src/main/java/com/codemind/cli/CodeMindCli.java
@@ -1,9 +1,34 @@
 package com.codemind.cli;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 /**
  * @Author qxy
  * @Date 2025/10/20 22:58
  * @Version 1.0
  */
+@Command(
+        name = "codemind",
+        description = "CodeMind CLI工具",
+        mixinStandardHelpOptions = true,
+        version = "CodeMind CLI 1.0",
+        subcommands = {
+
+        }
+)
 public class CodeMindCli {
+    @Option(
+            names = {"--config", "-c"},
+            description = "配置文件路径",
+            defaultValue = "config/application.properties"
+    )
+    private String configPath;
+
+    @Option(
+            names = {"--verbose", "-v"},
+            description = "详细输出模式"
+    )
+    private boolean verbose;
 }
