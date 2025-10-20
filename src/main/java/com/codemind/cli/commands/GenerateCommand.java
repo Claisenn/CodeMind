d05c135554eae33e2236615b2002b23f9345f61c
asus <qixinye-s@inspur.com>
2025-10-20 23:02:22 +0800

    添加代码生成和审查命令类

---
+++ src/main/java/com/codemind/cli/commands/GenerateCommand.java
@@ -0,0 +1,10 @@
+package com.codemind.cli.commands;
+
+/**
+ * @Author qxy
+ * @Date 2025/10/20 23:01
+ * @Version 1.0
+ */
+//代码生成
+public class GenerateCommand {
+}
---
+++ src/main/java/com/codemind/cli/commands/ReviewCommand.java
@@ -0,0 +1,10 @@
+package com.codemind.cli.commands;
+
+/**
+ * @Author qxy
+ * @Date 2025/10/20 23:00
+ * @Version 1.0
+ */
+//代码审查
+public class ReviewCommand {
+}
