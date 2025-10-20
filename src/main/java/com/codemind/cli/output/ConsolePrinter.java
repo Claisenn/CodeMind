01a34619a853b470606aef1a3bad154760e6e4bb
asus <qixinye-s@inspur.com>
2025-10-20 23:40:30 +0800

    添加CLI核心实现和配置加载方法

--- src/main/java/com/codemind/cli/output/ConsolePrinter.java
+++ src/main/java/com/codemind/cli/output/ConsolePrinter.java
@@ -7,4 +7,12 @@ package com.codemind.cli.output;
  */
 //控制台输出
 public class ConsolePrinter {
+    public static void printWarning(String s) {
+    }
+
+    public static void printError(String 未设置_openAIApiKey) {
+    }
+
+    public static void printInfo(String s) {
+    }
 }
